10e8a56c85e99eea432528beb8d43b40b814acad
mrajaonson <m.rajaonson@posteo.net>
2023-08-07 23:41:07 +0200

    add crypto when create user

--- src/main/java/fr/lpacsid/chat/utils/CryptoUtility.java
+++ src/main/java/fr/lpacsid/chat/utils/CryptoUtility.java
@@ -2,9 +2,9 @@ package fr.lpacsid.chat.utils;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class CryptoUtility {
-    public static String hashPassword(String string) {
+    public static String hashPassword(String password) {
         String salt = BCrypt.gensalt();
-        return BCrypt.hashpw(string, salt);
+        return BCrypt.hashpw(password, salt);
     }
 
     public static Boolean checkPassword(String password, String hashedPassword) {
